bc6c9b12f24b7d3168d47d84ecf3c21a77ef2f4c
Yogesh <yogeshheddure@gmail.com>
2026-07-09 12:20:41 +0530

    Create public route table using Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,6 +24,26 @@ resource "aws_subnet" "public" {
   }
 }
 
+####Route table
+
+resource "aws_route_table" "public" {
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name        = "public-route-table"
+    Environment = "Learning"
+    Project     = "Enterprise AWS Infrastructure"
+    Owner       = "Yogesh"
+  }
+}
+
 ###AWS INTERNET GATEWAY
 
 resource "aws_internet_gateway" "main" {
