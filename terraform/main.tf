7333fc29e74c9a33fb038837e9bcf75f561536b2
Yogesh <yogeshheddure@gmail.com>
2026-07-20 09:46:23 +0530

    Add EC2 instance deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -148,3 +148,27 @@ resource "aws_security_group" "web_sg" {
     Owner       = "Yogesh"
   }
 }
+
+####EC2 INSTANCE
+
+resource "aws_instance" "web" {
+
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+  key_name      = "enterprise-key"
+
+  subnet_id = aws_subnet.public.id
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name        = "enterprise-web-server"
+    Environment = "Learning"
+    Project     = "Enterprise AWS Infrastructure"
+    Owner       = "Yogesh"
+  }
+}
